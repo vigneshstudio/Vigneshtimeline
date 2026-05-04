af4b5a78adda3afbe068b9080bb2e7b0958243eb
vigneshstudio <vickykalai82880@gmail.com>
2026-05-04 22:17:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Vigneshtimeline/', // Add this line!
+})
+
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
